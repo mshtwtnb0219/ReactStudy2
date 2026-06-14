3529f912ced6434ec6fd21be4d01985f2ed0c359
masa <b77yellow@gmail.com>
2026-06-14 14:14:15 +0900

    pre ready

---
+++ react-app-study-record/src/Records.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+export const Records = () => {
+  // 記録
+  const [records, setRecords] = useState([]);
+  // エラー
+  const [error, setError] = useState("");
+  const disable = !error === "";
+
+  // 学習内容
+  const [studyContent, setStudyContent] = useState("");
+  // 学習時間
+  const [studyTime, setStudyTime] = useState(0);
+  // 合計時間の算出
+  const totalTime = records.reduce((sum, record) => {
+    return sum + parseInt(record.time);
+  }, 0);
+
+  // 学習内容の取得
+  const onChangeContent = (e) => setStudyContent(e.target.value);
+  // 学習時間の取得
+  const onChangeTime = (e) => setStudyTime(e.target.value);
+
+  // 学習時間の登録
+  const onClickRecord = () => {
+    if (studyContent === "" || !studyTime > 0) {
+      setError("入力されていない項目があります。");
+      return;
+    }
+    const record = {
+      id: crypto.randomUUID(),
+      title: studyContent,
+      time: studyTime,
+    };
+
+    setRecords([...records, record]);
+    setError("");
+  };
+  return (
+    <>
+      <h1>学習記録一覧</h1>
+      <div>
+        <form>
+          <p>
+            学習内容
+            <input onChange={onChangeContent}></input>
+          </p>
+          <p>
+            学習内容
+            <input
+              type="number"
+              value={studyTime}
+              onChange={onChangeTime}
+            ></input>
+            時間
+          </p>
+          <p>入力されている学習内容：{studyContent}</p>
+          <p>入力されている学習時間：{studyTime}</p>
+        </form>
+      </div>
+      <p disabled={disable}>{error}</p>
+
+      <ul>
+        {records.map((record) => (
+          <li key={record.id}>
+            {record.title} {record.time}時間
+          </li>
+        ))}
+      </ul>
+      <button onClick={onClickRecord}>登録</button>
+      <p>合計時間：{totalTime}/1000(h)</p>
+    </>
+  );
+};
